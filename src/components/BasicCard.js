aa6507cd7630233de3ed1e356a8d9534168c3bf8
AirinB <andreeamoraru090@gmail.com>
2022-03-06 18:34:31 +0100

    add ComplexCard

--- src/components/BasicCard.js
+++ src/components/BasicCard.js
@@ -5,44 +5,41 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import {CardMedia} from "@mui/material";
 
-const bull = (
-    <Box
-        component="span"
-        sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-    >
-        •
-    </Box>
-);
 
-const card = (
-    <React.Fragment>
-        <CardContent>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                Word of the Day
-            </Typography>
-            <Typography variant="h5" component="div">
-                be{bull}nev{bull}o{bull}lent
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                adjective
-            </Typography>
-            <Typography variant="body2">
-                well meaning and kindly.
-                <br />
-                {'"a benevolent smile"'}
-            </Typography>
-        </CardContent>
-        <CardActions>
-            <Button size="small">Learn More</Button>
-        </CardActions>
-    </React.Fragment>
-);
+export default function OutlinedCard(props) {
+    console.log(props.id, props.title)
 
-export default function OutlinedCard() {
     return (
         <Box sx={{ maxWidth: 200 }}>
-            <Card variant="outlined">{card}</Card>
+            <Card variant="outlined">
+                <React.Fragment>
+                    <CardMedia
+                        component="img"
+                        height="194"
+                        image={props.image}
+                        alt="Paella dish"
+                    />
+                    <CardContent>
+                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                            Word of the Day
+                        </Typography>
+                        <Typography variant="h5" component="div">
+                            {props.title}
+                        </Typography>
+                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                            adjective
+                        </Typography>
+                        <Typography variant="body2">
+                            {props.description}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">{props.rating}</Button>
+                    </CardActions>
+                </React.Fragment>
+            </Card>
         </Box>
     );
 }
--- src/components/CardsGrid.js
+++ src/components/CardsGrid.js
@@ -2,6 +2,8 @@ import OutlinedCard from './BasicCard'
 import * as React from "react";
 import styled from 'styled-components'
 import {useEffect, useState} from "react";
+import RecipeReviewCard from "./ComplexCard";
+// import OutlinedCard from "./BasicCard";
 
 const CardsGridStyled = styled.div`
   display: grid;
@@ -27,7 +29,11 @@ export default function CardsGrid() {
     return (
         <CardsGridStyled>
             {
-                values.map(()=><OutlinedCard/> )
+                // values.map((data)=><OutlinedCard id={data.id} title= {data.title} price={data.price}
+                //                                  description={data.description} image={data.image} rating={data.rating.rate}
+                //                                  count={data.rating.count}/> )
+
+                values.map((data)=><RecipeReviewCard image={data.image} /> )
             }
         </CardsGridStyled>
             );
